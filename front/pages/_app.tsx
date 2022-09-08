e5ad8384b6be4b78081b70b99150a7c13bc0c67e
채윤선 <yoonsun0809@gmail.com>
2022-09-08 14:33:22 +0900

    style : success에 #4411AA 넣어둠

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    success: {
+      main: "#4411AA",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider> 
 }
 
 export default MyApp
