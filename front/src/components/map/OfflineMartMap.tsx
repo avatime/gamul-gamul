26ac0a5945da258f08165a147bd2ddc5b8e8aa2d
정건우 <abcxj123@naver.com>
2022-09-13 10:40:55 +0900

    ﻿feat(map) : 카카오맵 테스트

---
+++ front/src/components/map/OfflineMartMap.tsx
@@ -0,0 +1,52 @@
+import styled from "@emotion/styled";
+import { useEffect } from "react";
+
+declare global {
+    interface Window {
+      kakao: any;
+    }
+  }
+
+interface MapProps {
+  latitude: any;
+  longitude: any;
+}
+
+function OfflineMartMap({ latitude, longitude }: MapProps) {
+  useEffect(() => {
+    const mapScript = document.createElement("script");
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById("map");
+        const options = {
+          center: new window.kakao.maps.LatLng(latitude, longitude),
+        };
+        const map = new window.kakao.maps.Map(container, options);
+        const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        });
+        marker.setMap(map);
+      });
+    };
+    mapScript.addEventListener("load", onLoadKakaoMap);
+
+    return () => mapScript.removeEventListener("load", onLoadKakaoMap);
+  }, [latitude, longitude]);
+
+  return (
+    <MapContainer id="map" />
+  );
+}
+
+const MapContainer = styled.div`
+  aspect-ratio: 320 / 220;
+`;
+
+export default OfflineMartMap;
