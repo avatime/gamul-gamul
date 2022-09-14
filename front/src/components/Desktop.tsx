e22f104b156e54306d26d160cba9be8ebe7f531c
이주영 <dlendjs93@gmail.com>
2022-09-14 17:10:49 +0900

    refactor: Mobile, Tablet, Desktop 컴포넌트 재정의
    useMediaQuery 사용하는 방식으로 하면 ssr이 안되는 것 같음!!

--- front/src/components/Desktop.tsx
+++ front/src/components/Desktop.tsx
@@ -1,15 +1,25 @@
+import styled from "@emotion/styled";
 import React, { FC, Fragment, useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
-interface IProps {
-  children: React.ReactNode;
-}
+// interface IProps {
+//   children: React.ReactNode;
+// }
 
-export const Desktop: FC<IProps> = ({ children }) => {
-  const [env, setEnv] = useState(false);
-  const isDesktop = useMediaQuery({ minWidth: 992 });
-  useEffect(() => {
-    setEnv(isDesktop);
-  }, [isDesktop]);
-  return <Fragment>{env && children}</Fragment>;
-};
+// export const Desktop: FC<IProps> = ({ children }) => {
+//   const [env, setEnv] = useState(false);
+//   const isDesktop = useMediaQuery({ minWidth: 992 });
+//   useEffect(() => {
+//     setEnv(isDesktop);
+//   }, [isDesktop]);
+//   return <Fragment>{env && children}</Fragment>;
+// };
+
+export const Desktop = styled.div`
+  @media (min-width: 992px) {
+    display: block;
+  }
+  @media (max-width: 991px) {
+    display: none;
+  }
+`;
