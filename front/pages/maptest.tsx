b796173c7098f263cca158690683ee0758c4165d
정건우 <abcxj123@naver.com>
2022-09-14 17:54:27 +0900

    feat(map) : 카카오맵 테스트2

--- front/pages/maptest.tsx
+++ front/pages/maptest.tsx
@@ -7,9 +7,15 @@ interface IProps {}
 const Maptest:FC<IProps> = (props) => {
     const [lat, setLat] = useState(0);
     const [lng, setLng] = useState(0);
+    const [arr, setArr] = useState([
+      {store_id : 1, name : 'test1', price: 500, latitude : 37.4553689, longitude : 126.7268268},
+      {store_id : 2, name : 'test2', price: 300, latitude : 37.4563689, longitude : 126.7244268},
+      {store_id : 3, name : 'test3', price: 600, latitude : 37.4573689, longitude : 126.7258268},
+      {store_id : 4, name : 'test4', price: 200, latitude : 37.4543689, longitude : 126.7248268}
+    ]);
 
     //const [location, setLocation] = useState(useGeolocation());
-    const location = useGeolocation();
+    const location : any = useGeolocation();
 
     // useEffect(() => {
     //     setTimeout(() => {
@@ -24,7 +30,7 @@ const Maptest:FC<IProps> = (props) => {
       <div className="App">
         {location.loaded ? JSON.stringify(location) : "Location data not available yet."}
       </div>
-      <OfflineMartMap latitude={37.5128064} longitude={127.0284288} />
+      <OfflineMartMap latitude={location.coordinates.lat} longitude={location.coordinates.lng} arr={arr} />
     </Fragment>
    )
  }
--- front/src/components/map/OfflineMartMap.tsx
+++ front/src/components/map/OfflineMartMap.tsx
@@ -10,9 +10,10 @@ declare global {
 interface MapProps {
   latitude: any;
   longitude: any;
+  arr : any;
 }
 
-function OfflineMartMap({ latitude, longitude }: MapProps) {
+function OfflineMartMap({ latitude, longitude, arr }: MapProps) {
   useEffect(() => {
     const mapScript = document.createElement("script");
 
@@ -26,13 +27,49 @@ function OfflineMartMap({ latitude, longitude }: MapProps) {
         const container = document.getElementById("map");
         const options = {
           center: new window.kakao.maps.LatLng(latitude, longitude),
+          level: 4,
         };
         const map = new window.kakao.maps.Map(container, options);
+        // const zoomControl = new window.kakao.maps.ZoomControl();
+        // map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+        //map.setZoomable(false);
+
+        const imageSrc = './select_temp.png',
+        imageSize = new window.kakao.maps.Size(30, 30),
+        imageOption = {offset: new window.kakao.maps.Point(0, 0)};
+
+        const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
         const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
+
         const marker = new window.kakao.maps.Marker({
           position: markerPosition,
+          image: markerImage
         });
         marker.setMap(map);
+        for(var i = 0; i<arr.length;i++) {
+          const imageSrc = './non_select_temp.png',
+          imageSize = new window.kakao.maps.Size(30, 30),
+          imageOption = {offset: new window.kakao.maps.Point(0, 0)};
+
+        const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+          const markerPosition = new window.kakao.maps.LatLng(arr[i].latitude, arr[i].longitude);
+          const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+          image: markerImage
+        });
+        var customOverlay = new window.kakao.maps.CustomOverlay({
+          map: map,
+          content: `<div style="padding:0 5px;background:#fff;">${arr[i].name}</div>`, 
+          position: new window.kakao.maps.LatLng(arr[i].latitude, arr[i].longitude), // 커스텀 오버레이를 표시할 좌표
+          xAnchor: 0, // 컨텐츠의 x 위치
+          yAnchor: -1.5 // 컨텐츠의 y 위치
+        });
+
+        marker.setMap(map);
+        customOverlay.setMap(map);
+        
+        }
       });
     };
     mapScript.addEventListener("load", onLoadKakaoMap);
@@ -46,7 +83,7 @@ function OfflineMartMap({ latitude, longitude }: MapProps) {
 }
 
 const MapContainer = styled.div`
-  aspect-ratio: 320 / 220;
+  width: 700px; height:350px; margin:0px 25px 0px;
 `;
 
 export default OfflineMartMap;
