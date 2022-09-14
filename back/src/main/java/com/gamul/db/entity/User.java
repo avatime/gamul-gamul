931dce7eae18a5034d6b21f12cabd06757ee1d41
이지나 <dlwlsk0428@naver.com>
2022-09-14 16:41:04 +0900

    feat: dockerfile 추가

--- back/src/main/java/com/gamul/db/entity/User.java
+++ back/src/main/java/com/gamul/db/entity/User.java
@@ -7,11 +7,10 @@ import lombok.Setter;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
+import javax.persistence.*;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * 유저 모델 정의
@@ -30,21 +29,23 @@ public class User extends BaseEntity {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     String password;
 
+    @Temporal(TemporalType.TIMESTAMP)
     @JsonProperty("created_time")
     @Column(updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private LocalDateTime createdTime;
+    private Date createdTime;
 
+    @Temporal(TemporalType.TIMESTAMP)
     @JsonProperty("update_time")
     @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private LocalDateTime updateTime;
+    private Date updateTime;
 
     @PrePersist
     public void onCreate() {
-        this.createdTime = LocalDateTime.now();
+        this.createdTime = Timestamp.valueOf(LocalDateTime.now());
     }
 
     @PreUpdate
     public void onUpdate() {
-        this.updateTime = LocalDateTime.now();
+        this.updateTime = Timestamp.valueOf(LocalDateTime.now());
     }
 }
