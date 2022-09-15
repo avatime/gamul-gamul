f4a6bf3e097d7986e720966e032b1c04acef0476
채윤선 <yoonsun0809@gmail.com>
2022-09-15 11:09:09 +0900

    Feat(SearchBar) : 껍데기추가

---
+++ front/src/components/SearchBar.tsx
@@ -0,0 +1,69 @@
+import { Box, IconButton, InputBase, Paper, Stack } from "@mui/material";
+import React, { FC } from "react";
+import { Desktop } from "./Desktop";
+import { Mobile } from "./Mobile";
+import { Tablet } from "./Tablet";
+import SearchIcon from "@mui/icons-material/Search";
+import styles from "../../styles/SearchBar.module.css";
+
+/* white : #fff
+   연한grey : #f5f5f4 */
+
+interface IProps {
+  color: string;
+}
+
+export const SearchBar: FC<IProps> = ({ color }) => {
+  return (
+    <div>
+      <Mobile>
+        <Box sx={{ marginLeft: "5%", marginRight: "5%", borderRadius: 20 }}>
+          <Stack
+            direction="row"
+            sx={{ borderRadius: 20, bgcolor: `${color}` }}
+            className={styles.stylesforMobile}
+          >
+            <InputBase sx={{ paddingLeft: 2 }} />
+            <Box sx={{ marginLeft: "50%" }}>
+              <IconButton color="success">
+                <SearchIcon />
+              </IconButton>
+            </Box>
+          </Stack>
+        </Box>
+      </Mobile>
+      <Desktop>
+        <Box sx={{ marginLeft: "5%", marginRight: "5%", borderRadius: 20 }}>
+          <Stack
+            direction="row"
+            sx={{ borderRadius: 20, bgcolor: `${color}` }}
+            className={styles.styles}
+          >
+            <InputBase sx={{ paddingLeft: 2 }} />
+            <Box sx={{ marginLeft: "50%" }}>
+              <IconButton color="success">
+                <SearchIcon />
+              </IconButton>
+            </Box>
+          </Stack>
+        </Box>
+      </Desktop>
+      <Tablet>
+        <Box sx={{ marginLeft: "5%", marginRight: "5%", borderRadius: 20 }}>
+          <Stack
+            direction="row"
+            sx={{ borderRadius: 20, bgcolor: `${color}` }}
+            className={styles.styles}
+          >
+            <InputBase sx={{ paddingLeft: 2 }} />
+            <Box sx={{ marginLeft: "50%" }}>
+              <IconButton color="success">
+                <SearchIcon />
+              </IconButton>
+            </Box>
+          </Stack>
+        </Box>
+      </Tablet>
+    </div>
+  );
+};
