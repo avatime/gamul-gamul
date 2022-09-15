607328076494ee5175fd5371704e1c2ce51223b7
채윤선 <yoonsun0809@gmail.com>
2022-09-15 18:00:31 +0900

    feat(HeaderBar) : Navbar 요소 고정 추가

--- front/src/components/HeaderBar.tsx
+++ front/src/components/HeaderBar.tsx
@@ -36,10 +36,12 @@ export const HeaderBar: FC<IProps> = ({ badgeContent }) => {
         </Stack>
       </Mobile>
       <Desktop>
-        <Stack direction="row" className={styles.styles}>
+      <Stack direction="row" className={styles.styles}>
           <Typography sx={{ fontWeight: "Bold" }}>가물가물</Typography>
+          <Box sx={{position:"fixed", left:250}}>
+            <SearchBar color="#f5f5f4" />
+          </Box>
           <Box sx={{ position: "fixed", right: 4 }}>
-            <SearchBar color="f5f5f4" />
             <IconButton>
               <Badge badgeContent={badgeContent} color="success">
                 <NotificationsIcon />
@@ -51,7 +53,9 @@ export const HeaderBar: FC<IProps> = ({ badgeContent }) => {
       <Tablet>
         <Stack direction="row" className={styles.styles}>
           <Typography sx={{ fontWeight: "Bold" }}>가물가물</Typography>
+          <Box sx={{position:"fixed", left:150}}>
             <SearchBar color="#f5f5f4" />
+          </Box>
           <Box sx={{ position: "fixed", right: 4 }}>
             <IconButton>
               <Badge badgeContent={badgeContent} color="success">
