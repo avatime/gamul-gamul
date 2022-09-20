ca4f9ae13d0335184c1950850d030fe4ebbbb57d
정건우 <abcxj123@naver.com>
2022-09-20 16:54:33 +0900

    feat(graph) : 그래프 컴포넌트 추가

--- front/src/components/graph/IngredientPriceComp.tsx
+++ front/src/components/graph/IngredientPriceComp.tsx
@@ -1,35 +1,31 @@
 import styled from "@emotion/styled";
-import { Box, ButtonBase } from "@mui/material";
+import { Box } from "@mui/material";
 import React, { FC } from "react";
-import { IngredientInfo } from "../../apis/responses/ingredientInfo";
-import { OfflineMartInfo } from '../../apis/responses/offlineMartInfo';
+import { IngredientDetailInfo } from "../../apis/responses/ingredientDetailInfo";
+import { CardContainer } from '../CardContainer';
+import IngredientPriceGraph from './IngredientPriceGraph';
 
-// 수정해야됨
 interface IProps {
-  offlineMartInfo?: OfflineMartInfo;
-  ingredientInfo?: IngredientInfo;
+  ingredientDetailInfo: IngredientDetailInfo;
+  title?: string;
 }
 
-export const IngredientPriceComp: FC<IProps> = ({ offlineMartInfo, ingredientInfo }) => {
+export const IngredientPriceComp: FC<IProps> = ({ ingredientDetailInfo, title = "물가 정보" }) => {
+  const priceTransitionInfo = ingredientDetailInfo.price_transition_info;
+  const pastPrice = priceTransitionInfo.before_price;
+  const pastVol = priceTransitionInfo.pastvol;
+  const todayPrice = priceTransitionInfo.price;
+  const todayVol = priceTransitionInfo.todayvol;
+  const quantity = ingredientDetailInfo.ingredient_info.quantity;
+  const unit = ingredientDetailInfo.ingredient_info.unit;
+
   return (
-      <Box
-        flex="1"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="row"
-      >
-        <Box position="relative" >
-          <p style={{fontSize: 12, fontWeight: "bold"}}>{offlineMartInfo?.name || "마트 이름"}</p>
-        </Box>
-        <Box flex={2} />
-        {offlineMartInfo ? <p style={{ fontSize: 12, margin: 3 }}>
-          {offlineMartInfo?.price || 0}원
-        </p>
-        : <p style={{ fontSize: 12, fontWeight: "bold"}}>가격({ingredientInfo?.quantity}
-        {ingredientInfo?.unit})</p>}
-        <Box flex={1} />
-        {offlineMartInfo ? <p style={{ fontSize: 10 }}>{offlineMartInfo?.distance || "거리"}m</p> : <p style={{ fontSize: 12, fontWeight: "bold"}}>거리</p>}
+      <Box>
+        <CardContainer title={title}>
+          <p style={{fontSize: 12, fontWeight: "bold"}}>어제 평균<p style={{color: !pastVol ? "black" : 0 < pastVol ? "red" : "blue"}}>{pastPrice}</p>원/{quantity}{unit}</p>
+          <p style={{fontSize: 12, fontWeight: "bold"}}>오늘 평균<p style={{color: !todayVol ? "black" : 0 < todayVol ? "red" : "blue"}}>{todayPrice}</p>원/{quantity}{unit}</p>
+          <IngredientPriceGraph priceTransitionInfo={priceTransitionInfo} inputWidth={370} inputHeight={300} ></IngredientPriceGraph>
+        </CardContainer>
       </Box>
   );
 };
