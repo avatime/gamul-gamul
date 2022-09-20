4c90ce87cdbe3105fe5d325ad2b09684e4be5946
이지나 <dlwlsk0428@naver.com>
2022-09-20 15:47:40 +0900

    set: gitignore 수정

--- back/src/main/java/com/gamul/api/controller/UserController.java
+++ back/src/main/java/com/gamul/api/controller/UserController.java
@@ -24,7 +24,6 @@ public class UserController {
     @ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.")
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 401, message = "인증 실패"),
             @ApiResponse(code = 404, message = "사용자 없음"),
             @ApiResponse(code = 500, message = "서버 오류")
     })
