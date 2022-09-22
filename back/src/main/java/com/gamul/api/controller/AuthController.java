6e784a40414241d1be4aae88c5fac7374a01e0f5
이지나 <dlwlsk0428@naver.com>
2022-09-22 16:32:23 +0900

    Fix: 로그인 로직 변경2

--- back/src/main/java/com/gamul/api/controller/AuthController.java
+++ back/src/main/java/com/gamul/api/controller/AuthController.java
@@ -40,14 +40,9 @@ public class AuthController {
     public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value="로그인 정보", required = true) UserLoginPostReq loginInfo) {
         String username = loginInfo.getUserName();
         String password = loginInfo.getPassword();
-        User user = new User();
 
-        try{
-            user = userService.getUserByUsername(username);
-        }
-        catch(Exception e){
-            return ResponseEntity.status(401).body(UserLoginPostRes.of(404, "Invalid User", null));
-        }
+        User user = userService.getUserByUsername(username);
+        if(user == null) return ResponseEntity.status(404).body(UserLoginPostRes.of(404, "Invalid User", null));
         // 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
         if(passwordEncoder.matches(password, user.getPassword())) {
             // 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
