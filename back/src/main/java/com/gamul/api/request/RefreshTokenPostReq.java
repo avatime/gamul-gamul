878adc8308ffb5a6b7e4738bd8e8dc098b463fb8
이지나 <dlwlsk0428@naver.com>
2022-09-22 12:54:35 +0900

    feat: refresh token 관련 파일 생성

---
+++ back/src/main/java/com/gamul/api/request/RefreshTokenPostReq.java
@@ -0,0 +1,19 @@
+package com.gamul.api.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ApiModel("RefreshTokenPostReq")
+@ToString
+public class RefreshTokenPostReq {
+
+    @JsonProperty("refresh_token")
+    @ApiModelProperty(name="JWT 인증 토큰", example="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN...")
+    String refreshToken;
+}
