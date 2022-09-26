bc64bae35861902de85a7310c945483c9611ad67
정건우 <abcxj123@naver.com>
2022-09-26 18:01:04 +0900

    ﻿feat(my-recipe) : 나의 요리법 페이지 추가(수정 필요)

--- front/pages/my-recipe.tsx
+++ front/pages/my-recipe.tsx
@@ -3,14 +3,19 @@ import { NextPage } from "next";
 import { Desktop } from "../src/components/Desktop";
 import { Mobile } from "../src/components/Mobile";
 import { Tablet } from "../src/components/Tablet";
+import { MyRecipeComp } from "../src/components/templates/MyRecipeComp";
 import styles from "../styles/Page.module.css";
-import { Page } from '../src/components/Page';
+import { MyRecipeInfo } from '../src/apis/responses/myRecipeInfo';
+import { ApiClient } from '../src/apis/apiClient';
+import { getCookie } from '../src/utils/cookie';
 
-interface IProps {}
+interface IProps {
+  myRecipeList : MyRecipeInfo[];
+}
 
-const MyRecipePage: NextPage<IProps> = (props) => {
+const MyRecipePage: NextPage<IProps> = ({ myRecipeList }) => {
   return (
-    <Page>
+    <Box className="page-background">
       <Desktop>
         <Box className={styles.PageforDesktop}></Box>
       </Desktop>
@@ -18,10 +23,24 @@ const MyRecipePage: NextPage<IProps> = (props) => {
         <Box className={styles.PageforTablet}></Box>
       </Tablet>
       <Mobile>
-        <Box className={styles.PageforMobile}></Box>
+        <Box className={styles.PageforMobile}>
+          <MyRecipeComp myRecipeList={myRecipeList} />
+        </Box>
       </Mobile>
-    </Page>
+    </Box>
   );
 };
 
 export default MyRecipePage;
+
+export const getStaticProps = async () => {
+  const apiClient = ApiClient.getInstance();
+  const userName = getCookie("userName");
+  const List = apiClient.getMyRecipeList(userName);
+
+  return {
+    props: {
+      myRecipeList: JSON.parse(JSON.stringify(List)),
+    },
+  };
+};
