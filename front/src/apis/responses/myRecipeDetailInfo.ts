dd7c2ef9d8ed5028b43acc89c2f58b1ab48be728
정건우 <abcxj123@naver.com>
2022-09-27 18:39:02 +0900

    ﻿feat(myrecipe) : 나만의 요리법 목록, 상세정보 화면 추가

--- front/src/apis/responses/myRecipeDetailInfo.ts
+++ front/src/apis/responses/myRecipeDetailInfo.ts
@@ -5,4 +5,6 @@ export interface MyRecipeDetailInfo {
   total_price: number; // 식재료 총 금액
   ingredient_list: IngredientInfo[];
   price_transition_info: PriceTransitionInfo;
+  image_path: string;
+  name: string;
 }
