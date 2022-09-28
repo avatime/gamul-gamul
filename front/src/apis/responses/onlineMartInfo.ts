b43061d46492c1446811be64017b35febd48ffb1
채윤선 <yoonsun0809@gmail.com>
2022-09-28 11:53:02 +0900

    Feat(onlineMartetComp) : 1차완성

--- front/src/apis/responses/onlineMartInfo.ts
+++ front/src/apis/responses/onlineMartInfo.ts
@@ -1,6 +1,7 @@
 export interface OnlineMartInfo {
   image_path: string; // 쇼핑몰 이미지 경로
-  name: string; // 마트 이름
+  mall_name: string; // 마트 이름
   price: number; // 가격
   url: string; // 링크 url
+  name : string; //실제 팔고 있는 이름
 }
