20af229d624e806e403dc6324de65c9502277346
정건우 <abcxj123@naver.com>
2022-09-30 10:18:56 +0900

    refactor(graph) : 그래프 컴포넌트 수정 1차

--- front/src/components/IngredientPriceGraph.tsx
+++ front/src/components/IngredientPriceGraph.tsx
@@ -3,24 +3,27 @@ import dynamic from "next/dynamic";
 import { PriceTransitionInfo } from "../apis/responses/priceTransitionInfo";
 import moment from "moment";
 
+type Type = "line" | "bar"
+
 interface IProps {
   priceTransitionInfo: PriceTransitionInfo;
   inputWidth: any;
   inputHeight: number;
-  type: string;
+  type: Type;
 }
 
 moment.locale("ko");
 
 const IngredientPriceGraph: FC<IProps> = ({ priceTransitionInfo, inputWidth, inputHeight, type }) => {
   const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+  const Chart2 = dynamic(() => import("react-apexcharts"), { ssr: false });
   const retailMax = Math.max(...priceTransitionInfo.retailsales.daily.map((v) => v.price));
   const wholeMax = Math.max(...priceTransitionInfo.wholesales.daily.map((v) => v.price));
   const max = Math.floor(Math.max(retailMax, wholeMax) * 1.2 / 100) * 100;
 
   return (
     <div>
-      <Chart
+      {type === "line" && <Chart
         height={inputHeight}
         width={inputWidth}
         series={[
@@ -35,7 +38,7 @@ const IngredientPriceGraph: FC<IProps> = ({ priceTransitionInfo, inputWidth, inp
         ]}
         options={{
           chart: {
-            type: type === "line" ? "line" : "bar",
+            type: "line",
             dropShadow: {
               enabled: true,
               color: "#000",
@@ -93,7 +96,82 @@ const IngredientPriceGraph: FC<IProps> = ({ priceTransitionInfo, inputWidth, inp
           },
         }}
       />
-    </div>
+      }
+      {type === "bar" && <Chart2
+        height={inputHeight}
+        width={inputWidth}
+        series={[
+          {
+            name: "소매",
+            data: priceTransitionInfo.retailsales.daily.map((v) => v.price),
+          },
+          {
+            name: "도매",
+            data: priceTransitionInfo.wholesales.daily.map((v) => v.price),
+          },
+        ]}
+        options={{
+          chart: {
+            type: "bar",
+            toolbar: {
+              show: false,
+            },
+          },
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '55%',
+            },
+          },
+          colors: ["#4411AA", "#163ED9"],
+          dataLabels: {
+            enabled: false,
+          },
+          stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+          },          
+          title: {
+            text: "가격 변화",
+            align: "left",
+          },
+          // grid: {
+          //   borderColor: "#e7e7e7",
+          //   row: {
+          //     colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          //     opacity: 0.5,
+          //   },
+          // },
+          markers: {
+            size: 1,
+          },
+          xaxis: {
+            categories: priceTransitionInfo.retailsales.daily.map((v) =>
+              moment(v.date).format("MM.D")
+            ),
+          },
+          yaxis: {
+            title: {
+              text: `가격(${priceTransitionInfo.retailsales.daily[0].quantity}${priceTransitionInfo.retailsales.daily[0].unit})`,
+            },
+            min: 0,
+            max: max,
+          },
+          legend: {
+            position: "top",
+            horizontalAlign: "right",
+            floating: true,
+            offsetY: -20,
+            offsetX: -5,
+          },  
+          fill: {
+            opacity: 1
+          },
+        }}
+      />
+      }
+      </div>
   );
 };
 
---
+++ front/src/components/templates/IngredientBarGraphComp.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useEffect, useState } from 'react'
+import { Box } from '@mui/system';
+import { Grid } from '@mui/material';
+import { IngredientInfo } from '../../apis/responses/ingredientInfo';
+import IngredientPriceGraph from '../IngredientPriceGraph';
+import { PriceTransitionInfo } from '../../apis/responses/priceTransitionInfo';
+
+interface IProps {
+    ingredientInfo: IngredientInfo;
+    priceTransitionInfo: PriceTransitionInfo;
+}
+
+export const IngredientBarGraphComp:FC<IProps> = ({ ingredientInfo, priceTransitionInfo }) => {
+
+  return (
+    <Grid container sx={{margin: "15px 0px 0px 0px"}}>
+        <Grid item xs={2} sx={{display: "flex", flexDirection: "column", justifyContent: "center", }}>
+            <p style={{fontSize: 16, display: "flex", justifyContent: "flex-end"}}>{ingredientInfo.name}</p>
+            <p style={{fontSize: 22, fontWeight: "bold", display: "flex", justifyContent: "flex-end"}}>{ingredientInfo.price.toLocaleString()}</p>
+            <p style={{fontSize: 14, color: "inherit", display: "flex", justifyContent: "flex-end"}}>원/{ingredientInfo.quantity}{ingredientInfo.unit}</p>
+        </Grid>
+        <Grid item xs={10}>
+            <IngredientPriceGraph priceTransitionInfo={priceTransitionInfo} inputWidth="90%" inputHeight={250} type="bar" />
+        </Grid>
+    </Grid>
+   )
+ }
