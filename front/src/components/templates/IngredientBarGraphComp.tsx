dccd114812ee540cdcac4498284c0884eb8b1fd0
정건우 <abcxj123@naver.com>
2022-09-30 14:41:32 +0900

    temp(graph) : 그래프 임시 저장

--- front/src/components/templates/IngredientBarGraphComp.tsx
+++ front/src/components/templates/IngredientBarGraphComp.tsx
@@ -8,6 +8,7 @@ import { PriceTransitionInfo } from '../../apis/responses/priceTransitionInfo';
 interface IProps {
     ingredientInfo: IngredientInfo;
     priceTransitionInfo: PriceTransitionInfo;
+    isExternerImage?: boolean;
 }
 
 export const IngredientBarGraphComp:FC<IProps> = ({ ingredientInfo, priceTransitionInfo }) => {
@@ -15,12 +16,20 @@ export const IngredientBarGraphComp:FC<IProps> = ({ ingredientInfo, priceTransit
   return (
     <Grid container sx={{margin: "15px 0px 0px 0px"}}>
         <Grid item xs={2} sx={{display: "flex", flexDirection: "column", justifyContent: "center", }}>
+            {/* <Image 
+                width="32"
+                height="32"
+                src={}
+                alt={ingredientInfo.name}
+                style={{ borderRadius: 32 }}
+                unoptimized={isExternalImage}            
+            /> */}
             <p style={{fontSize: 16, display: "flex", justifyContent: "flex-end"}}>{ingredientInfo.name}</p>
             <p style={{fontSize: 22, fontWeight: "bold", display: "flex", justifyContent: "flex-end"}}>{ingredientInfo.price.toLocaleString()}</p>
             <p style={{fontSize: 14, color: "inherit", display: "flex", justifyContent: "flex-end"}}>원/{ingredientInfo.quantity}{ingredientInfo.unit}</p>
         </Grid>
         <Grid item xs={10}>
-            <IngredientPriceGraph priceTransitionInfo={priceTransitionInfo} inputWidth="90%" inputHeight={250} type="bar" />
+            <IngredientPriceGraph priceTransitionInfo={priceTransitionInfo} inputWidth="95%" inputHeight={250} type="bar" />
         </Grid>
     </Grid>
    )
