408ebc6691edd57d3455833fa6258a82479be6f4
rrkcl7733@naver.com <rrkcl7733@naver.com>
2022-10-01 16:36:04 +0900

    feat: recipe table 추가

---
+++ bigdata/hive/recipe.java
@@ -0,0 +1,455 @@
+// ORM class for table 'recipe'
+// WARNING: This class is AUTO-GENERATED. Modify at your own risk.
+//
+// Debug information:
+// Generated date: Sat Sep 24 18:18:17 KST 2022
+// For connector: org.apache.sqoop.manager.MySQLManager
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+import com.cloudera.sqoop.lib.JdbcWritableBridge;
+import com.cloudera.sqoop.lib.DelimiterSet;
+import com.cloudera.sqoop.lib.FieldFormatter;
+import com.cloudera.sqoop.lib.RecordParser;
+import com.cloudera.sqoop.lib.BooleanParser;
+import com.cloudera.sqoop.lib.BlobRef;
+import com.cloudera.sqoop.lib.ClobRef;
+import com.cloudera.sqoop.lib.LargeObjectLoader;
+import com.cloudera.sqoop.lib.SqoopRecord;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class recipe extends SqoopRecord  implements DBWritable, Writable {
+  private final int PROTOCOL_VERSION = 3;
+  public int getClassFormatVersion() { return PROTOCOL_VERSION; }
+  public static interface FieldSetterCommand {    void setField(Object value);  }  protected ResultSet __cur_result_set;
+  private Map<String, FieldSetterCommand> setters = new HashMap<String, FieldSetterCommand>();
+  private void init0() {
+    setters.put("id", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        recipe.this.id = (Long)value;
+      }
+    });
+    setters.put("information", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        recipe.this.information = (String)value;
+      }
+    });
+    setters.put("name", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        recipe.this.name = (String)value;
+      }
+    });
+    setters.put("thumbnail", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        recipe.this.thumbnail = (String)value;
+      }
+    });
+  }
+  public recipe() {
+    init0();
+  }
+  private Long id;
+  public Long get_id() {
+    return id;
+  }
+  public void set_id(Long id) {
+    this.id = id;
+  }
+  public recipe with_id(Long id) {
+    this.id = id;
+    return this;
+  }
+  private String information;
+  public String get_information() {
+    return information;
+  }
+  public void set_information(String information) {
+    this.information = information;
+  }
+  public recipe with_information(String information) {
+    this.information = information;
+    return this;
+  }
+  private String name;
+  public String get_name() {
+    return name;
+  }
+  public void set_name(String name) {
+    this.name = name;
+  }
+  public recipe with_name(String name) {
+    this.name = name;
+    return this;
+  }
+  private String thumbnail;
+  public String get_thumbnail() {
+    return thumbnail;
+  }
+  public void set_thumbnail(String thumbnail) {
+    this.thumbnail = thumbnail;
+  }
+  public recipe with_thumbnail(String thumbnail) {
+    this.thumbnail = thumbnail;
+    return this;
+  }
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof recipe)) {
+      return false;
+    }
+    recipe that = (recipe) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.information == null ? that.information == null : this.information.equals(that.information));
+    equal = equal && (this.name == null ? that.name == null : this.name.equals(that.name));
+    equal = equal && (this.thumbnail == null ? that.thumbnail == null : this.thumbnail.equals(that.thumbnail));
+    return equal;
+  }
+  public boolean equals0(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof recipe)) {
+      return false;
+    }
+    recipe that = (recipe) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.information == null ? that.information == null : this.information.equals(that.information));
+    equal = equal && (this.name == null ? that.name == null : this.name.equals(that.name));
+    equal = equal && (this.thumbnail == null ? that.thumbnail == null : this.thumbnail.equals(that.thumbnail));
+    return equal;
+  }
+  public void readFields(ResultSet __dbResults) throws SQLException {
+    this.__cur_result_set = __dbResults;
+    this.id = JdbcWritableBridge.readLong(1, __dbResults);
+    this.information = JdbcWritableBridge.readString(2, __dbResults);
+    this.name = JdbcWritableBridge.readString(3, __dbResults);
+    this.thumbnail = JdbcWritableBridge.readString(4, __dbResults);
+  }
+  public void readFields0(ResultSet __dbResults) throws SQLException {
+    this.id = JdbcWritableBridge.readLong(1, __dbResults);
+    this.information = JdbcWritableBridge.readString(2, __dbResults);
+    this.name = JdbcWritableBridge.readString(3, __dbResults);
+    this.thumbnail = JdbcWritableBridge.readString(4, __dbResults);
+  }
+  public void loadLargeObjects(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void loadLargeObjects0(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void write(PreparedStatement __dbStmt) throws SQLException {
+    write(__dbStmt, 0);
+  }
+
+  public int write(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeLong(id, 1 + __off, -5, __dbStmt);
+    JdbcWritableBridge.writeString(information, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(name, 3 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(thumbnail, 4 + __off, 12, __dbStmt);
+    return 4;
+  }
+  public void write0(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeLong(id, 1 + __off, -5, __dbStmt);
+    JdbcWritableBridge.writeString(information, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(name, 3 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(thumbnail, 4 + __off, 12, __dbStmt);
+  }
+  public void readFields(DataInput __dataIn) throws IOException {
+this.readFields0(__dataIn);  }
+  public void readFields0(DataInput __dataIn) throws IOException {
+    if (__dataIn.readBoolean()) { 
+        this.id = null;
+    } else {
+    this.id = Long.valueOf(__dataIn.readLong());
+    }
+    if (__dataIn.readBoolean()) { 
+        this.information = null;
+    } else {
+    this.information = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.name = null;
+    } else {
+    this.name = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.thumbnail = null;
+    } else {
+    this.thumbnail = Text.readString(__dataIn);
+    }
+  }
+  public void write(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeLong(this.id);
+    }
+    if (null == this.information) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, information);
+    }
+    if (null == this.name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, name);
+    }
+    if (null == this.thumbnail) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, thumbnail);
+    }
+  }
+  public void write0(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeLong(this.id);
+    }
+    if (null == this.information) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, information);
+    }
+    if (null == this.name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, name);
+    }
+    if (null == this.thumbnail) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, thumbnail);
+    }
+  }
+  private static final DelimiterSet __outputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  public String toString() {
+    return toString(__outputDelimiters, true);
+  }
+  public String toString(DelimiterSet delimiters) {
+    return toString(delimiters, true);
+  }
+  public String toString(boolean useRecordDelim) {
+    return toString(__outputDelimiters, useRecordDelim);
+  }
+  public String toString(DelimiterSet delimiters, boolean useRecordDelim) {
+    StringBuilder __sb = new StringBuilder();
+    char fieldDelim = delimiters.getFieldsTerminatedBy();
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(information==null?"null":information, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(name==null?"null":name, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(thumbnail==null?"null":thumbnail, delimiters));
+    if (useRecordDelim) {
+      __sb.append(delimiters.getLinesTerminatedBy());
+    }
+    return __sb.toString();
+  }
+  public void toString0(DelimiterSet delimiters, StringBuilder __sb, char fieldDelim) {
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(information==null?"null":information, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(name==null?"null":name, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(thumbnail==null?"null":thumbnail, delimiters));
+  }
+  private static final DelimiterSet __inputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  private RecordParser __parser;
+  public void parse(Text __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharSequence __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(byte [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(char [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(ByteBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  private void __loadFromFields(List<String> fields) {
+    Iterator<String> __it = fields.listIterator();
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Long.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.information = null; } else {
+      this.information = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.name = null; } else {
+      this.name = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.thumbnail = null; } else {
+      this.thumbnail = __cur_str;
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  private void __loadFromFields0(Iterator<String> __it) {
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Long.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.information = null; } else {
+      this.information = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.name = null; } else {
+      this.name = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.thumbnail = null; } else {
+      this.thumbnail = __cur_str;
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  public Object clone() throws CloneNotSupportedException {
+    recipe o = (recipe) super.clone();
+    return o;
+  }
+
+  public void clone0(recipe o) throws CloneNotSupportedException {
+  }
+
+  public Map<String, Object> getFieldMap() {
+    Map<String, Object> __sqoop$field_map = new HashMap<String, Object>();
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("information", this.information);
+    __sqoop$field_map.put("name", this.name);
+    __sqoop$field_map.put("thumbnail", this.thumbnail);
+    return __sqoop$field_map;
+  }
+
+  public void getFieldMap0(Map<String, Object> __sqoop$field_map) {
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("information", this.information);
+    __sqoop$field_map.put("name", this.name);
+    __sqoop$field_map.put("thumbnail", this.thumbnail);
+  }
+
+  public void setField(String __fieldName, Object __fieldVal) {
+    if (!setters.containsKey(__fieldName)) {
+      throw new RuntimeException("No such field:"+__fieldName);
+    }
+    setters.get(__fieldName).setField(__fieldVal);
+  }
+
+}
