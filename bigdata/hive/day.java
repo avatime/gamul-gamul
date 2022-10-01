e1db7b89761019aa7c47df9bf54ce0e242115946
rrkcl7733@naver.com <rrkcl7733@naver.com>
2022-10-01 16:12:08 +0900

    feat: day table  추가

---
+++ bigdata/hive/day.java
@@ -0,0 +1,519 @@
+// ORM class for table 'day'
+// WARNING: This class is AUTO-GENERATED. Modify at your own risk.
+//
+// Debug information:
+// Generated date: Thu Sep 29 10:46:57 KST 2022
+// For connector: org.apache.sqoop.manager.MySQLManager
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+import com.cloudera.sqoop.lib.JdbcWritableBridge;
+import com.cloudera.sqoop.lib.DelimiterSet;
+import com.cloudera.sqoop.lib.FieldFormatter;
+import com.cloudera.sqoop.lib.RecordParser;
+import com.cloudera.sqoop.lib.BooleanParser;
+import com.cloudera.sqoop.lib.BlobRef;
+import com.cloudera.sqoop.lib.ClobRef;
+import com.cloudera.sqoop.lib.LargeObjectLoader;
+import com.cloudera.sqoop.lib.SqoopRecord;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class day extends SqoopRecord  implements DBWritable, Writable {
+  private final int PROTOCOL_VERSION = 3;
+  public int getClassFormatVersion() { return PROTOCOL_VERSION; }
+  public static interface FieldSetterCommand {    void setField(Object value);  }  protected ResultSet __cur_result_set;
+  private Map<String, FieldSetterCommand> setters = new HashMap<String, FieldSetterCommand>();
+  private void init0() {
+    setters.put("id", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        day.this.id = (Long)value;
+      }
+    });
+    setters.put("datetime", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        day.this.datetime = (String)value;
+      }
+    });
+    setters.put("price", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        day.this.price = (Integer)value;
+      }
+    });
+    setters.put("type", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        day.this.type = (Integer)value;
+      }
+    });
+    setters.put("ingredient_id", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        day.this.ingredient_id = (Long)value;
+      }
+    });
+  }
+  public day() {
+    init0();
+  }
+  private Long id;
+  public Long get_id() {
+    return id;
+  }
+  public void set_id(Long id) {
+    this.id = id;
+  }
+  public day with_id(Long id) {
+    this.id = id;
+    return this;
+  }
+  private String datetime;
+  public String get_datetime() {
+    return datetime;
+  }
+  public void set_datetime(String datetime) {
+    this.datetime = datetime;
+  }
+  public day with_datetime(String datetime) {
+    this.datetime = datetime;
+    return this;
+  }
+  private Integer price;
+  public Integer get_price() {
+    return price;
+  }
+  public void set_price(Integer price) {
+    this.price = price;
+  }
+  public day with_price(Integer price) {
+    this.price = price;
+    return this;
+  }
+  private Integer type;
+  public Integer get_type() {
+    return type;
+  }
+  public void set_type(Integer type) {
+    this.type = type;
+  }
+  public day with_type(Integer type) {
+    this.type = type;
+    return this;
+  }
+  private Long ingredient_id;
+  public Long get_ingredient_id() {
+    return ingredient_id;
+  }
+  public void set_ingredient_id(Long ingredient_id) {
+    this.ingredient_id = ingredient_id;
+  }
+  public day with_ingredient_id(Long ingredient_id) {
+    this.ingredient_id = ingredient_id;
+    return this;
+  }
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof day)) {
+      return false;
+    }
+    day that = (day) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.datetime == null ? that.datetime == null : this.datetime.equals(that.datetime));
+    equal = equal && (this.price == null ? that.price == null : this.price.equals(that.price));
+    equal = equal && (this.type == null ? that.type == null : this.type.equals(that.type));
+    equal = equal && (this.ingredient_id == null ? that.ingredient_id == null : this.ingredient_id.equals(that.ingredient_id));
+    return equal;
+  }
+  public boolean equals0(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof day)) {
+      return false;
+    }
+    day that = (day) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.datetime == null ? that.datetime == null : this.datetime.equals(that.datetime));
+    equal = equal && (this.price == null ? that.price == null : this.price.equals(that.price));
+    equal = equal && (this.type == null ? that.type == null : this.type.equals(that.type));
+    equal = equal && (this.ingredient_id == null ? that.ingredient_id == null : this.ingredient_id.equals(that.ingredient_id));
+    return equal;
+  }
+  public void readFields(ResultSet __dbResults) throws SQLException {
+    this.__cur_result_set = __dbResults;
+    this.id = JdbcWritableBridge.readLong(1, __dbResults);
+    this.datetime = JdbcWritableBridge.readString(2, __dbResults);
+    this.price = JdbcWritableBridge.readInteger(3, __dbResults);
+    this.type = JdbcWritableBridge.readInteger(4, __dbResults);
+    this.ingredient_id = JdbcWritableBridge.readLong(5, __dbResults);
+  }
+  public void readFields0(ResultSet __dbResults) throws SQLException {
+    this.id = JdbcWritableBridge.readLong(1, __dbResults);
+    this.datetime = JdbcWritableBridge.readString(2, __dbResults);
+    this.price = JdbcWritableBridge.readInteger(3, __dbResults);
+    this.type = JdbcWritableBridge.readInteger(4, __dbResults);
+    this.ingredient_id = JdbcWritableBridge.readLong(5, __dbResults);
+  }
+  public void loadLargeObjects(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void loadLargeObjects0(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void write(PreparedStatement __dbStmt) throws SQLException {
+    write(__dbStmt, 0);
+  }
+
+  public int write(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeLong(id, 1 + __off, -5, __dbStmt);
+    JdbcWritableBridge.writeString(datetime, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeInteger(price, 3 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeInteger(type, 4 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeLong(ingredient_id, 5 + __off, -5, __dbStmt);
+    return 5;
+  }
+  public void write0(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeLong(id, 1 + __off, -5, __dbStmt);
+    JdbcWritableBridge.writeString(datetime, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeInteger(price, 3 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeInteger(type, 4 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeLong(ingredient_id, 5 + __off, -5, __dbStmt);
+  }
+  public void readFields(DataInput __dataIn) throws IOException {
+this.readFields0(__dataIn);  }
+  public void readFields0(DataInput __dataIn) throws IOException {
+    if (__dataIn.readBoolean()) { 
+        this.id = null;
+    } else {
+    this.id = Long.valueOf(__dataIn.readLong());
+    }
+    if (__dataIn.readBoolean()) { 
+        this.datetime = null;
+    } else {
+    this.datetime = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.price = null;
+    } else {
+    this.price = Integer.valueOf(__dataIn.readInt());
+    }
+    if (__dataIn.readBoolean()) { 
+        this.type = null;
+    } else {
+    this.type = Integer.valueOf(__dataIn.readInt());
+    }
+    if (__dataIn.readBoolean()) { 
+        this.ingredient_id = null;
+    } else {
+    this.ingredient_id = Long.valueOf(__dataIn.readLong());
+    }
+  }
+  public void write(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeLong(this.id);
+    }
+    if (null == this.datetime) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, datetime);
+    }
+    if (null == this.price) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.price);
+    }
+    if (null == this.type) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.type);
+    }
+    if (null == this.ingredient_id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeLong(this.ingredient_id);
+    }
+  }
+  public void write0(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeLong(this.id);
+    }
+    if (null == this.datetime) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, datetime);
+    }
+    if (null == this.price) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.price);
+    }
+    if (null == this.type) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.type);
+    }
+    if (null == this.ingredient_id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeLong(this.ingredient_id);
+    }
+  }
+  private static final DelimiterSet __outputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  public String toString() {
+    return toString(__outputDelimiters, true);
+  }
+  public String toString(DelimiterSet delimiters) {
+    return toString(delimiters, true);
+  }
+  public String toString(boolean useRecordDelim) {
+    return toString(__outputDelimiters, useRecordDelim);
+  }
+  public String toString(DelimiterSet delimiters, boolean useRecordDelim) {
+    StringBuilder __sb = new StringBuilder();
+    char fieldDelim = delimiters.getFieldsTerminatedBy();
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(datetime==null?"null":datetime, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(price==null?"null":"" + price, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(type==null?"null":"" + type, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(ingredient_id==null?"null":"" + ingredient_id, delimiters));
+    if (useRecordDelim) {
+      __sb.append(delimiters.getLinesTerminatedBy());
+    }
+    return __sb.toString();
+  }
+  public void toString0(DelimiterSet delimiters, StringBuilder __sb, char fieldDelim) {
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(datetime==null?"null":datetime, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(price==null?"null":"" + price, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(type==null?"null":"" + type, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(ingredient_id==null?"null":"" + ingredient_id, delimiters));
+  }
+  private static final DelimiterSet __inputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  private RecordParser __parser;
+  public void parse(Text __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharSequence __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(byte [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(char [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(ByteBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  private void __loadFromFields(List<String> fields) {
+    Iterator<String> __it = fields.listIterator();
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Long.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.datetime = null; } else {
+      this.datetime = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.price = null; } else {
+      this.price = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.type = null; } else {
+      this.type = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.ingredient_id = null; } else {
+      this.ingredient_id = Long.valueOf(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  private void __loadFromFields0(Iterator<String> __it) {
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Long.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.datetime = null; } else {
+      this.datetime = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.price = null; } else {
+      this.price = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.type = null; } else {
+      this.type = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.ingredient_id = null; } else {
+      this.ingredient_id = Long.valueOf(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  public Object clone() throws CloneNotSupportedException {
+    day o = (day) super.clone();
+    return o;
+  }
+
+  public void clone0(day o) throws CloneNotSupportedException {
+  }
+
+  public Map<String, Object> getFieldMap() {
+    Map<String, Object> __sqoop$field_map = new HashMap<String, Object>();
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("datetime", this.datetime);
+    __sqoop$field_map.put("price", this.price);
+    __sqoop$field_map.put("type", this.type);
+    __sqoop$field_map.put("ingredient_id", this.ingredient_id);
+    return __sqoop$field_map;
+  }
+
+  public void getFieldMap0(Map<String, Object> __sqoop$field_map) {
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("datetime", this.datetime);
+    __sqoop$field_map.put("price", this.price);
+    __sqoop$field_map.put("type", this.type);
+    __sqoop$field_map.put("ingredient_id", this.ingredient_id);
+  }
+
+  public void setField(String __fieldName, Object __fieldVal) {
+    if (!setters.containsKey(__fieldName)) {
+      throw new RuntimeException("No such field:"+__fieldName);
+    }
+    setters.get(__fieldName).setField(__fieldVal);
+  }
+
+}
