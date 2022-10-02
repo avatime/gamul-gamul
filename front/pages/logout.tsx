1d8f1db998b96d9df76011036cec79e840f591ff
채윤선 <yoonsun0809@gmail.com>
2022-10-02 16:02:56 +0900

    Style (withdraw, logout) : CSS 수정

--- front/pages/logout.tsx
+++ front/pages/logout.tsx
@@ -4,7 +4,11 @@ import { useRouter } from "next/router";
 import React, { FC } from "react";
 import { ButtonFill } from "../src/components/button/ButtonFill";
 import { removeCookie } from "../src/utils/cookie";
-import { Page } from '../src/components/Page';
+import { Page } from "../src/components/Page";
+import { Desktop } from "../src/components/Desktop";
+import { Mobile } from "../src/components/Mobile";
+import { Tablet } from "../src/components/Tablet";
+import styles from "../styles/Page.module.css";
 
 interface IProps {}
 
@@ -17,35 +21,105 @@ const LogoutPage: NextPage<IProps> = (props) => {
   };
   return (
     <Page>
-      <Box
-        sx={{
-          position: "absolute",
-          width: "60vw",
-          left: 0,
-          right: 0,
-          top: "30%",
-          maxWidth: "500px",
-          margin: "auto",
-        }}
-      >
-        <Stack direction="column" sx={{ alignItems: "center" }}>
-          <>
-            <Typography sx={{ fontWeight: "bold", fontSize: "large" }}>
-              로그아웃 하시겠습니까?
-            </Typography>
-            <Box p={2} />
-            <ButtonFill
-              text={"네"}
-              onClick={logout}
-              height={"50px"}
-              width={"60vw"}
-              maxWidth={"500px"}
-              fontSize={""}
-              disabled={false}
-            />
-          </>
-        </Stack>
-      </Box>
+      <Mobile>
+        <Box className={styles.PageforMobile}>
+          <Box
+            sx={{
+              position: "absolute",
+              width: "60vw",
+              left: 0,
+              right: 0,
+              top: "30%",
+              maxWidth: "500px",
+              margin: "auto",
+            }}
+          >
+            <Stack direction="column" sx={{ alignItems: "center" }}>
+              <>
+                <Typography sx={{ fontWeight: "bold", fontSize: "large" }}>
+                  로그아웃 하시겠습니까?
+                </Typography>
+                <Box p={2} />
+                <ButtonFill
+                  text={"네"}
+                  onClick={logout}
+                  height={"50px"}
+                  width={"60vw"}
+                  maxWidth={"500px"}
+                  fontSize={""}
+                  disabled={false}
+                />
+              </>
+            </Stack>
+          </Box>
+        </Box>
+      </Mobile>
+      <Tablet>
+        <Box className={styles.PageforTablet}>
+          <Box
+            sx={{
+              position: "absolute",
+              width: "60vw",
+              left: 150,
+              right: 0,
+              top: "30%",
+              maxWidth: "500px",
+              margin: "auto",
+            }}
+          >
+            <Stack direction="column" sx={{ alignItems: "center" }}>
+              <>
+                <Typography sx={{ fontWeight: "bold", fontSize: "large" }}>
+                  로그아웃 하시겠습니까?
+                </Typography>
+                <Box p={2} />
+                <ButtonFill
+                  text={"네"}
+                  onClick={logout}
+                  height={"50px"}
+                  width={"60vw"}
+                  maxWidth={"500px"}
+                  fontSize={""}
+                  disabled={false}
+                />
+              </>
+            </Stack>
+          </Box>
+        </Box>
+      </Tablet>
+      <Desktop>
+        <Box className={styles.PageforDesktop}>
+          <Box
+            sx={{
+              position: "absolute",
+              width: "60vw",
+              left: 200,
+              right: 0,
+              top: "30%",
+              maxWidth: "500px",
+              margin: "auto",
+            }}
+          >
+            <Stack direction="column" sx={{ alignItems: "center" }}>
+              <>
+                <Typography sx={{ fontWeight: "bold", fontSize: "large" }}>
+                  로그아웃 하시겠습니까?
+                </Typography>
+                <Box p={2} />
+                <ButtonFill
+                  text={"네"}
+                  onClick={logout}
+                  height={"50px"}
+                  width={"60vw"}
+                  maxWidth={"500px"}
+                  fontSize={""}
+                  disabled={false}
+                />
+              </>
+            </Stack>
+          </Box>
+        </Box>
+      </Desktop>
     </Page>
   );
 };
