a9547148627790cbfc3d567665aa7a2569146a32
이지나 <dlwlsk0428@naver.com>
2022-10-02 16:10:05 +0900

    ﻿feat: 마이레시피 완성

--- back/src/main/java/com/gamul/db/repository/YearRepository.java
+++ back/src/main/java/com/gamul/db/repository/YearRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
  * 연도별 가격 모델 관련 디비 쿼리 생성을 위한 JPA Query Method 인터페이스 정의.
  */
 public interface YearRepository extends JpaRepository<Year, Long> {
-    public Year findTop1ByIngredientIdOrderByDatetimeDesc(Long ingredientId);
     public List<Year> findTop10ByIngredientIdOrderByDatetimeDesc(Long ingredientId);
+    public Year findTop1ByIngredientIdOrderByDatetimeDesc(Long ingredientId);
 }
