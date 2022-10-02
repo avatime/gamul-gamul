29907e1bed9ee3e7059881d78bdfdcaf2896ffa7
letgodchan0 <letgodchan0@gmail.com>
2022-10-03 00:30:54 +0900

    feat: 검색 api

--- back/src/main/java/com/gamul/db/repository/IngredientRepository.java
+++ back/src/main/java/com/gamul/db/repository/IngredientRepository.java
@@ -1,6 +1,7 @@
 package com.gamul.db.repository;
 
 import com.gamul.db.entity.Ingredient;
+import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.Optional;
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     Optional<List<Ingredient>> findAllByHighClass(int highClass);
     Optional<Ingredient> findById(Long ingredientId);
+    Optional<List<Ingredient>> findByMidClassLike(String keyword);
 
 }
