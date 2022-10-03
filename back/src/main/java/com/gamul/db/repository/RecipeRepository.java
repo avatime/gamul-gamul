0107a1276e9fcb9bcfbd658e2a56de59b6c8f695
이지나 <dlwlsk0428@naver.com>
2022-10-03 23:00:17 +0900

    feat: 레시피

--- back/src/main/java/com/gamul/db/repository/RecipeRepository.java
+++ back/src/main/java/com/gamul/db/repository/RecipeRepository.java
@@ -1,11 +1,9 @@
 package com.gamul.db.repository;
 
 import com.gamul.db.entity.Recipe;
-import org.hibernate.sql.Select;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
