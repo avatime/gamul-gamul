66a68740b13a8a8a12fc387d92ea3471e677ce28
채윤선 <yoonsun0809@gmail.com>
2022-10-03 14:17:43 +0900

    ﻿Style(식재료 상세 조회) : 스타일 수정

--- front/src/components/OnlineMarketInfoComp.tsx
+++ front/src/components/OnlineMarketInfoComp.tsx
@@ -1,5 +1,5 @@
 import { Stack, Table, TableCell, TableRow } from "@mui/material";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { CardContainer } from "./CardContainer";
 import { OnlineMartInfo } from "../../src/apis/responses/onlineMartInfo";
 import Link from "next/link";
@@ -9,9 +9,9 @@ import path from "path";
 import styles from "../../styles/OnlineMarketInfoComp.module.css";
 import Lottie from "lottie-react";
 import cart from "../../public/assets/cart2.json";
+
 interface IProps {
   onlineMartInfo: OnlineMartInfo[];
-  width: string;
   iconSize: string;
 }
 
@@ -20,11 +20,13 @@ interface IProps {
  * @function @OnlineMarketInfoComp
  **/
 
-export const OnlineMarketInfoComp: FC<IProps> = ({ onlineMartInfo, width, iconSize }) => {
+export const OnlineMarketInfoComp: FC<IProps> = ({ onlineMartInfo, iconSize }) => {
+  
   return (
     <div>
-      <CardContainer title={"온라인"} style={{ width }}>
-        <Table>
+      <CardContainer title={"온라인"}>
+        <Table sx={{ tableLayout: "fixed" }}>
+
           {onlineMartInfo.map((row, idx) => (
             <TableRow
               key={idx}
@@ -33,20 +35,27 @@ export const OnlineMarketInfoComp: FC<IProps> = ({ onlineMartInfo, width, iconSi
               }}
               sx={{ cursor: "pointer" }}
               className={styles.hover}
+              
             >
-              <TableCell>{row.name}</TableCell>
+              <TableCell
+                sx={{
+                  textOverflow: "ellipsis",
+                  whiteSpace: "nowrap",
+                  width: "50%",
+                  overflow: "hidden",
+                }}
+              >
+                {row.name}
+              </TableCell>
+             
               <TableCell>
-                <Stack direction="row" sx={{ alignItems: "center" }}>
-                  {/* <Lottie
-                    animationData={cart}
-                    loop={true}
-                    autoPlay={false}
-                    style={{ width: iconSize, height: iconSize }}
-                  /> */}
+                <Stack direction="row" sx={{ alignItems: "center", whiteSpace: "nowrap",width: "30%", }}>
                   {row.mall_name}
                 </Stack>
               </TableCell>
-              <TableCell>{row.price.toLocaleString()}원</TableCell>
+              <TableCell sx={{ alignItems: "right", whiteSpace: "nowrap" , width: "20%",}}>
+                {row.price.toLocaleString()}원
+              </TableCell>
             </TableRow>
           ))}
         </Table>
