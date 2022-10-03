0687f931f99db9d63069c921fd85f31c3de47540
이지나 <dlwlsk0428@naver.com>
2022-10-04 00:51:00 +0900

    feat: 마이 레시피 상세조회

--- back/src/main/java/com/gamul/db/repository/DayRepository.java
+++ back/src/main/java/com/gamul/db/repository/DayRepository.java
@@ -4,6 +4,7 @@ import com.gamul.db.entity.Day;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 일자별 가격 모델 관련 디비 쿼리 생성을 위한 JPA Query Method 인터페이스 정의.
@@ -17,7 +18,7 @@ public interface DayRepository extends JpaRepository<Day, Long> {
     public Day findTop1ByIngredientIdAndTypeOrderByDatetimeDesc(Long ingredientId, int type);
 
     public Day findTop1ByIngredientIdAndTypeOrderByDatetimeDescUnit(Long ingredientId, int type);
-    public List<Day> findTop10ByIngredientIdAndTypeAndUnitOrderByDatetimeDesc(Long ingredientId, int type, String unit);
+    public Optional<List<Day>> findTop10ByIngredientIdAndTypeAndUnitOrderByDatetimeDesc(Long ingredientId, int type, String unit);
 
     List<Day> findTop7ByIngredientIdAndTypeAndUnitOrderByDatetimeDesc(Long ingredientId, int type, String unit);
 }
